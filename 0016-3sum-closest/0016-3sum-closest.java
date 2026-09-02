6d6690eacdad022650e18390d5621f87a6a7a819
vtu27517-create <vtu27517@veltech.edu.in>
2026-09-02 10:23:44 +0530

    Time: 16 ms (93.16%), Space: 45.4 MB (81.33%) - LeetHub

---
+++ 0016-3sum-closest/0016-3sum-closest.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+
+class Solution {
+    public int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
+
+        int closestSum = nums[0] + nums[1] + nums[2];
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            int left = i + 1;
+            int right = nums.length - 1;
+
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+
+                // Update closest sum
+                if (Math.abs(sum - target) < Math.abs(closestSum - target)) {
+                    closestSum = sum;
+                }
+
+                // Move pointers
+                if (sum < target) {
+                    left++;
+                } else if (sum > target) {
+                    right--;
+                } else {
+                    return sum; // Exact match
+                }
+            }
+        }
+
+        return closestSum;
+    }
+}
