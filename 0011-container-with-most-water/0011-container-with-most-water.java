264bcb15c078bbd665056f41e49288facdf8eafa
vtu27517-create <vtu27517@veltech.edu.in>
2026-09-02 10:19:57 +0530

    Time: 5 ms (83.21%), Space: 77.1 MB (85.77%) - LeetHub

---
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int maxArea(int[] height) {
+        int left = 0;
+        int right = height.length - 1;
+        int maxWater = 0;
+
+        while (left < right) {
+            int width = right - left;
+            int currentHeight = Math.min(height[left], height[right]);
+
+            int area = width * currentHeight;
+            maxWater = Math.max(maxWater, area);
+
+            // Move the pointer with the smaller height
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return maxWater;
+    }
+}
