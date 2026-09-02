3284cbfef4399be394a17f71c6b1aa231a312cef
vtu27517-create <vtu27517@veltech.edu.in>
2026-09-02 10:21:29 +0530

    Time: 4 ms (52.49%), Space: 46.6 MB (36.96%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -0,0 +1,33 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int romanToInt(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+
+        int result = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            int current = map.get(s.charAt(i));
+
+            // If the current value is smaller than the next value,
+            // subtract it.
+            if (i < s.length() - 1 &&
+                current < map.get(s.charAt(i + 1))) {
+                result -= current;
+            } else {
+                result += current;
+            }
+        }
+
+        return result;
+    }
+}
